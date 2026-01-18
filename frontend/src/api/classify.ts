d406e4b588a30d0d88517c92f761f21bf79164ff
Gustavo Gomes <lgglima@gmail.com>
2026-01-18 13:16:30 -0300

    change url to test deploy

--- frontend/src/api/classify.ts
+++ frontend/src/api/classify.ts
@@ -1,7 +1,7 @@
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5001/classify"
-    : "/api/classify";
+    : "/classify";
 
 export async function classifyEmail(
   text: string,
